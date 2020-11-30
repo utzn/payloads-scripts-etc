4bc7c5930e2a0bdafc140550312a458e16102ada
Utz Nisslmueller <accounts@nisslmueller.me>
2020-11-30 12:47:22 +0100

    Created apt update script

---
+++ bash/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+test_server="8.8.8.8"
+timeout="1"
+
+echo "Testing network connection..."
+
+conn_test() {
+    ping $test_server -c 1 -w $timeout > ping.tmp
+    sleep $timeout
+    if [ "$(wc -l ping.tmp)" == "6 ping.tmp" ]; then
+        echo 0  
+    else
+        echo 1
+    fi
+    #rm ping.tmp
+}
+
+res=$(conn_test)
+
+if [ "$res" == "0" ]; then
+    echo "Connection established."
+    apt-get update > /dev/null
+    apt-get upgrade -y > /dev/null
+    echo "Done! All installed packages have been updated."
+else
+    echo "Could not connect to $test_server. Check connection."
+fi
